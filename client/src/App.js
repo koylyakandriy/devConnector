0d6b00cabc886559234ed5905cb3307dc602f26e
koylyakandriy <koylyakandriy@gmail.com>
2019-09-03 20:19:41 +0300

    Start create client side Main/Regist/Login pages

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Registration from "./components/auth/Registration";
+import Login from "./components/auth/Login";
+
+import './App.css';
+
+const App = () => (
+	<Router>
+		<Fragment>
+			<Navbar/>
+			<Route exact path="/" component={ Landing }/>
+			<section className="container">
+				<Switch>
+					<Route exact path="/registration" component={ Registration }/>
+					<Route exact path="/login" component={ Login }/>
+				</Switch>
+			</section>
+		</Fragment>
+	</Router>
+
+);
+
+export default App;
